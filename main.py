512ec22a570673016ce0cd0315c56f6bb24a87b7
Mindok(Daniell) <cole_daniel@rocketmail.com>
2024-03-18 19:06:56 +0100

    added loading unsolved sodukos from txt file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import numpy as np
 
-for i in np.arange(0,10):
-    print(i)
+sodukoFile = open("soduko.txt","r")
+sodukoString = "".join(sodukoFile.readlines()).replace("\n","")
+print(sodukoString)
+sodukoMartix = np.matrix(sodukoString)
+print(sodukoMartix)
+# sodukoArray = np.array(sodukoMatrix)
+# print(sodukoArray)
+
+
